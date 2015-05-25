8dd459f64b1a1a7fee640165140871435d0cf73f
Lauren <lhspiegel@gmail.com>
2015-05-25 10:27:17 -0700

    Change server.js so database does not drop when server restarted

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ require('./config/middleware.js')(app, express, passport);
 
 app.set('port', (process.env.PORT || 3000));
 
-models.sequelize.sync({force: true})
+models.sequelize.sync()   //Add {force: true} in sync if want to drop data.  
 .done(function(){
   app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'));
